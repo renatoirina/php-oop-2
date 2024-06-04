9035fb9fcf14dacb46a180455365152163ddf401
renatoirina <irinarenato793@gmail.com>
2024-06-04 23:03:14 +0200

    feat: viewed the informations with cards

--- index.php
+++ index.php
@@ -1,26 +1,67 @@
 <?php
-require_once 'models/category.php';
-require_once 'models/product.php';
-require_once 'models/food.php';
-require_once 'models/toy.php';
-require_once 'models/bed.php';
+require_once 'models/Category.php';
+require_once 'models/Product.php';
+require_once 'models/Food.php';
+require_once 'models/Toy.php';
+require_once 'models/Bed.php';
 
 // Creazione di una categoria di esempio
-$categoryDog = new Category("Dog", "dogIcon.jpg");
-$categoryCat = new Category("Cat", "catIcon.jpg");
+$categoryDog = new Category("Dog", "🐶");
+$categoryCat = new Category("Cat", "🐱");
 
 // Creazione di prodotti di esempio
-$productFood = new Food("Dog Food", 19.99, "dog_food.jpg", $categoryDog, "2024-12-31");
-$productToy = new Toy("Cat Toy", 9.99, "cat_toy.jpg", $categoryCat, "Plastic");
-$productBed = new Bed("Dog Bed", 49.99, "dog_bed.jpg", $categoryDog, "Large", "Cotton");
-
-// Stampo per verificare
-var_dump($categoryDog);
-var_dump($categoryCat);
-var_dump($productFood);
-var_dump($productToy);
-var_dump($productBed);
-var_dump($productFood->getDetails());
-var_dump($productToy->getDetails());
-var_dump($productBed->getDetails());
+$products = [
+    new Food("Dog Food", 19.99, "./img/dogFood1.jpg", $categoryDog, "2024-12-31"),
+    new Toy("Cat Toy", 9.99, "./img/catToy1.jpeg", $categoryCat, "Plastic"),
+    new Bed("Dog Bed", 49.99, "./img/dogBed1.jpeg", $categoryDog, "Large", "Cotton")
+];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Pet Shop</title>
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <?php foreach ($products as $product) : ?>
+                <?php $details = $product->getDetails(); ?>
+                <div class="col-md-4">
+                    <div class="card mb-4 shadow-sm">
+                        <img src="<?= $details['image']; ?>" class="card-img-top" alt="<?= $details['title']; ?>">
+                        <div class="card-body text-center">
+                            <h5 class="card-title"><?= $details['title']; ?></h5>
+                            <p class="card-text"><?= $details['category']; ?> <?= $details['icon']; ?></p>
+                            <p class="card-text">Price: €<?= number_format($details['price'], 2); ?></p>
+                            <p class="card-text">Type: <?= $details['type']; ?></p>
+                            <?php if (isset($details['expirationDate'])) : ?>
+                                <p class="card-text">Expiration Date: <?= $details['expirationDate']; ?></p>
+                            <?php endif; ?>
+                            <?php if (isset($details['material'])) : ?>
+                                <p class="card-text">Material: <?= $details['material']; ?></p>
+                            <?php endif; ?>
+                            <?php if (isset($details['size'])) : ?>
+                                <p class="card-text">Size: <?= $details['size']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
+
+    <!-- BOOTSTRAP -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+</html>
